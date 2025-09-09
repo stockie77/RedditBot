e9e2328b1aabb3ea7ab72a2cf68ca7dda51a53b7
stockie77 <60014960+stockie77@users.noreply.github.com>
2025-09-09 19:14:16 +0200

    Add Reddit crawler and export functionality for acronym analysis

--- NASDAQ-Filter.py
+++ NASDAQ-Filter.py
@@ -1,10 +1,15 @@
 import pandas as pd
-
+import os
+cwd=""
 # Pfad zur Excel-Datei
-file_path = r'C:\Users\IHR-PFAD\Nasdaq1.xlsx'
+cwd = os.getcwd()
 
+cwd=cwd+"\\NYSE1.xlsx"
+print(f'pfad ist {cwd}')
+#cwd=r'C:\Users\stockie\Documents\GitHub\RedditBot\Nasdaq1.xlsx'
+#cwd=r'C:\Users\stockie\Documents\GitHub\RedditBot\Nasdaq1.xlsx'
 # Excel-Datei laden
-df = pd.read_excel(file_path)
+df = pd.read_excel(cwd)
 
 # Duplikate basierend auf der zweiten Spalte (Unternehmen) identifizieren
 # Nur die erste Zeile jedes Unternehmens behalten
@@ -14,7 +19,7 @@ unique_df = df.drop_duplicates(subset=df.columns[1], keep='first')
 duplicates_df = df[df.duplicated(subset=df.columns[1], keep='first')]
 
 # Neue Excel-Datei mit zwei Blättern erstellen
-with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+with pd.ExcelWriter(cwd, engine='openpyxl') as writer:
     unique_df.to_excel(writer, sheet_name='Tabelle1', index=False)
     duplicates_df.to_excel(writer, sheet_name='Tabelle2', index=False)
 
--- latenode_udemy.py
+++ latenode_udemy.py
@@ -1,9 +1,9 @@
 
 import requests
 
-    client_id=,
-    client_secret=,
-    password=,
+    client_id=os.getenv('REDDIT_CLIENT_ID'),
+    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+    password=os.getenv('REDDIT_CLIENT_SECRET'),,
     user_agent=,
     username=,
 
